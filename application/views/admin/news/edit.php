7521a6097ad9d56c5619ea7eb7b80a76c949e329
pringgojs <odyinggo@gmail.com>
2018-06-25 23:13:34 +0700

    dev

--- application/views/admin/news/edit.php
+++ application/views/admin/news/edit.php
@@ -279,104 +279,101 @@ function showF(divId) {
 
        
 // <!-- MAPS GOOOGLE -->
-      
-      var marker, myCircle, map;
-      function initialized() {
-        var myLatlng = new google.maps.LatLng(37.7699298, -122.4469157);
-        var mapOptions = {
-          zoom: 12,
-          center: myLatlng,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
-        map = new google.maps.Map(document.getElementById('map_canvas'), 
-            mapOptions);
+/**
+  var marker, myCircle, map;
+  function initialized() {
+    var myLatlng = new google.maps.LatLng(37.7699298, -122.4469157);
+    var mapOptions = {
+      zoom: 12,
+      center: myLatlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    map = new google.maps.Map(document.getElementById('map_canvas'), 
+        mapOptions);
 
-        google.maps.event.addListener(map, 'click', function(event){
-            addMarker(event.latLng);
-            $("#langitude").val(event.latLng.lat());
-            $("#longitude").val(event.latLng.lng());
-        });
+    google.maps.event.addListener(map, 'click', function(event){
+        addMarker(event.latLng);
+        $("#langitude").val(event.latLng.lat());
+        $("#longitude").val(event.latLng.lng());
+    });
+  }
+
+  function addMarker(latLng){       
+      //clear the previous marker and circle.
+      if(marker != null){
+          marker.setMap(null);
+          //myCircle.setMap(null);
       }
 
-      function addMarker(latLng){       
-        //clear the previous marker and circle.
-        if(marker != null){
-            marker.setMap(null);
-           //myCircle.setMap(null);
-        }
+      marker = new google.maps.Marker({
+          position: latLng,
+          map: map,
+          draggable:true
+      });
+        
+  }
+  // sercing location
+  function initAutocomplete() {
 
-        marker = new google.maps.Marker({
-            position: latLng,
-            map: map,
-            draggable:true
-        });
-         
-    }
-    // sercing location
-    function initAutocomplete() {
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-  // Create the search box and link it to the UI element.
-  var input = document.getElementById('pac-input');
-  var searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
-  });
-
-  var markers = [];
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener('places_changed', function() {
-    var places = searchBox.getPlaces();
-
-    if (places.length == 0) {
-      return;
-    }
-
-    // Clear out the old markers.
-    markers.forEach(function(marker) {
-      marker.setMap(null);
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+      searchBox.setBounds(map.getBounds());
     });
-    markers = [];
 
-    // For each place, get the icon, name and location.
-    var bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function() {
+      var places = searchBox.getPlaces();
 
-      // Create a marker for each place.
-      markers.push(new google.maps.Marker({
-        map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
-
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
+      if (places.length == 0) {
+        return;
       }
+
+      // Clear out the old markers.
+      markers.forEach(function(marker) {
+        marker.setMap(null);
+      });
+      markers = [];
+
+      // For each place, get the icon, name and location.
+      var bounds = new google.maps.LatLngBounds();
+      places.forEach(function(place) {
+        var icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
+
+        // Create a marker for each place.
+        markers.push(new google.maps.Marker({
+          map: map,
+          icon: icon,
+          title: place.name,
+          position: place.geometry.location
+        }));
+
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
     });
-    map.fitBounds(bounds);
-  });
-}
+  }
  //IP KEy
-    </script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC9rojO6haHjOAMyKVciedVOhqNFZ5C3Ek&libraries=places&callback=initAutocomplete"
-         async defer></script>
-    
-    <script>
-    /* JS FOR ADD PAGES & EDIT PAGES
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC9rojO6haHjOAMyKVciedVOhqNFZ5C3Ek&libraries=places&callback=initAutocomplete" async defer>
+ 
+ /* JS FOR ADD PAGES & EDIT PAGES
 ================================ */
 
 var editCustomLine = false;
